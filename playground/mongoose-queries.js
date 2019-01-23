dbd153080f0c1152acf3160efe886266c68ef719
Tyler Taylor <vikingtyty@gmail.com>
2019-01-23 09:48:26 -0600

    Add test cases for GET /todos/:id

--- playground/mongoose-queries.js
+++ playground/mongoose-queries.js
@@ -2,6 +2,7 @@ const { ObjectID } = require('mongodb')
 
 const { mongoose } = require('./../server/db/mongoose')
 const { Todo } = require('./../server/models/todo')
+const { User } = require('./../server/models/user')
 
 let id = '5c488577f2c039eb5aa76e2d'
 
